7026967fbb3c3e1b4548df7d20b6fe2019d96cfb
krishnch <krishna.chaitanya@gess.ethz.ch>
2022-09-01 17:05:45 +0200

    inference script for MRI data with 1 or 2 channel inputs of fat and water images

--- train_model/inference_script.sh
+++ train_model/inference_script.sh
@@ -5,8 +5,11 @@ cd <git_repo>/train_model/
 # activate tensorflow environment (env_name)
 source activate <env_name>
 
-#inference for any new test image
-python inference.py --ip_path=<path_of_input_image> --out_path=<path_to_save_predicted_segmentation_mask>
+#inference for any new test image - only fat image - 1 channel input
+python inference_mri.py --ip_path_fat=<path_of_input_fat_image> --out_path=<path_to_save_predicted_segmentation_mask>
+
+#inference for any new test image - both fat & water images as 2 channel input
+#python inference_mri.py --ip_path_fat=<path_of_input_fat_image>  --ip_path_water=<path_of_input_water_image> --out_path=<path_to_save_predicted_segmentation_mask>
 
 #to run inference on train, test and validation sets used for model training;
 #model trained with affine augmentations
