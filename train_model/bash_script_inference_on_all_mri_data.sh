84e5c4dfd9dee78a1b6af02c9f1a49d92e98cb9c
krishnch <krishna.chaitanya@gess.ethz.ch>
2022-09-07 10:59:43 +0200

    bash script to run inference on all MRI subjects

---
+++ train_model/bash_script_inference_on_all_mri_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ip_path='/usr/bmicnas01/data-biwi-01/bmicdatasets-originals/Originals/USZ/bodyfat_orig/top_down_nifti'
+for pid in {1..70}
+do
+   for timept in 1 2
+   do 
+      echo "patient id $pid, time point $timept"
+      FILE=$ip_path'/top_down_niftyp'$pid'_z'$timept'_f.nii.gz'
+      #echo $FILE
+      if [ -f "$FILE" ]; then
+         echo "$FILE exists. Running Inference on it."
+         python inference_mri.py --ip_path_fat=$FILE --patient_id=$pid --time_pt=$timept
+      fi
+   done
+done
