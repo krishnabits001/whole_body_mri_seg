4b6b379a1cca3412adeecd9e543021bf8e80e211
krishnch <krishna.chaitanya@gess.ethz.ch>
2022-09-09 11:29:17 +0200

    inference details to run on all CT images

--- train_model/bash_script_inference_on_all_mri_data.sh
+++ train_model/bash_script_inference_on_all_mri_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#inference on all MRI images
 ip_path='/usr/bmicnas01/data-biwi-01/bmicdatasets-originals/Originals/USZ/bodyfat_orig/top_down_nifti'
 for pid in {1..70}
 do
@@ -15,3 +16,18 @@ do
       fi
    done
 done
+
+#inference on all CT images
+##ip_path='/usr/bmicnas01/data-biwi-01/krishnch/projects/bodyfat_seg/fin_git_repo/new_scan_data/ct/sl_im/'
+##for pid in {91..100}
+##do
+##   echo "patient id $pid"
+##   FILE=$ip_path'/p'$pid'.nii.gz'
+##   #echo $FILE
+##   if [ -f "$FILE" ]; then
+##      echo "$FILE exists. Running Inference on it."
+##      python inference_ct.py --ip_path_fat=$FILE --patient_id=$pid 
+##      #give out_path here to save the predicted masks for all subjects or define it once inside inference_mri.py file.
+##      #python inference_ct.py --ip_path_ct=$FILE --out_path=<directory to save all predicted masks> --patient_id=$pid
+##   fi
+##done
